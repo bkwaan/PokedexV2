d4d942c663a5112c9d8e900b7428ccb0a42dbff3
bkwaan <bkwan15@my.bcit.ca>
2021-09-10 21:10:04 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const api = require('./api');
+
+// Connecting Database
+connectDB();
+
+// Using api folder
+app.use('/api',api);
+
+
+const PORT = process.env.PORT || 5000;
+ 
+app.listen(PORT, () => console.log('this server started on port ' + PORT));
+
+
