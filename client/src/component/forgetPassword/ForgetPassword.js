a29ca508a9245d0a5782271321b39cb45aa9201d
bkwaan <bkwan15@my.bcit.ca>
2021-10-09 16:59:16 -0700

    finalized styling the forgot password modal

--- client/src/component/forgetPassword/ForgetPassword.js
+++ client/src/component/forgetPassword/ForgetPassword.js
@@ -1,22 +1,36 @@
 import { Modal, Row, Col } from "react-bootstrap";
 import PokeBall from "../../images/pokeball.png";
+import { useState } from "react";
 
 const ForgetPassword = () => {
+  const [onHide, setOnHide] = useState(false);
+  const [onShow, setOnShow] = useState(true);
+
+  const onClose = () => {
+    setOnHide(true);
+    setOnShow(false);
+  };
+  
   return (
     <div>
-      <Modal size="lg" show={true}>
+      <Modal size="lg" show={onShow} onHide={onHide}>
         <div className="forgetPassCont">
+          <div className="closeIcon">
+            <i class="bi bi-x-lg" onClick={onClose}></i>
+          </div>
           <div className="pokeImgCont">
             <img className="pokeBallImg" src={PokeBall} />
           </div>
           <p className="forgotPassText">Forgot your password?</p>
           <p className="forgetPassDialog">
-            Enter your registered email to receive instructions on recovering
-            your account.
+            Enter your registered email to receive <br /> instructions on
+            recovering your account.
           </p>
           <form>
             <input className="emailInput" />
-            <button className="resetBtn">Submit</button>
+            <button className="resetBtn" placeholder="Email">
+              Submit
+            </button>
           </form>
         </div>
       </Modal>
