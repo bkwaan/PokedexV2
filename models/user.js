f2e8fca980d73f63c7f7bab8061baa59d5370847
bkwaan <bkwan15@my.bcit.ca>
2021-10-31 16:36:00 -0700

    added user model

--- models/user.js
+++ models/user.js
@@ -2,27 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    UserName: {
-        type: String,
-        required: true
-    },
-    Email:{
-        type: String,
-        required: true,
-    },
-    Password: {
-        type: String,
-        required: true
-    },
-    FirstName: {
-        type: String,
-        required: true
-    },
-    LastName: {
-        type: String,
-        required: true
-    },
-
-})
+  UserName: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+  FirstName: {
+    type: String,
+    required: true,
+  },
+  LastName: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = User = mongoose.model("User", UserSchema);
