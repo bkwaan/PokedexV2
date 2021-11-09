23b28b3f762e18ff103e3dcad4973dc49a70c577
bkwaan <bkwan15@my.bcit.ca>
2021-11-08 18:09:03 -0800

    fixed typos on user route

--- api/user.js
+++ api/user.js
@@ -1,8 +1,34 @@
-const express = require('express');
+const express = require("express");
 const bcrypt = require("bcrypt");
-const Router = express.Router();
+const router = express.Router();
 const Users = require("../models/user");
 
 const saltRounds = 10;
+
+router.post("/SignUp", async (req, res) => {
+  var { FirstName, LastName, UserName, Email, Password } = req.body;
+  try {
+    let user = await Users.find({
+      $or: [{ Email: Email }, { UserName: UserName }],
+    }).exec();
+    if (!user) {
+      res.status(409).send("Username or Email already exists");
+    }
+
+    let user1 = new Users({
+      UserName: UserName,
+      Email: Email,
+      Password: Password,
+      FirstName: FirstName,
+      LastName: LastName,
+    });
+
+    let user2 = await user1.save();
+    res.status(201).send(user2);
+
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
