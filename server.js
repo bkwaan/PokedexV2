a1a5485efb7626c58748b8d641610cdcb2c106ff
bkwaan <bkwan15@my.bcit.ca>
2022-03-17 18:23:10 -0700

    modified server.js for hosting purposes

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-const api = require('./api');
-const path = require('path')
-
-
-     
-
+const connectDB = require("./config/db");
+const api = require("./api");
+const path = require("path");
 
 // Connecting Database
 connectDB();
 
 //MiddleWare
 app.use(express.json({ extended: false }));
-app.use(express.static(path.join(__dirname, 'profilePics')))
-
+app.use(express.static(path.join(__dirname, "profilePics")));
 
 // Using api folder
-app.use('/api',api);
+app.use("/api", api);
 
+// Serve Staic Assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set Static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
- 
-app.listen(PORT, () => console.log('this server started on port ' + PORT));
 
-
+app.listen(PORT, () => console.log("this server started on port " + PORT));
