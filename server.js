a7dffd5e9dadbf8a884477c406f21f923ab543b5
bkwaan <bkwan15@my.bcit.ca>
2022-03-17 19:35:01 -0700

    made changes

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ if (process.env.NODE_ENV === "production") {
 }
 const PORT = process.env.PORT || 5000;
 
-
+console.log("Working")
 app.listen(PORT, () => console.log("this server started on port " + PORT));
