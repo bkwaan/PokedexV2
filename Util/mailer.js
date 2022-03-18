03fedc76e5c3a85abc21534fdaeec951ce7128e5
bkwaan <bkwan15@my.bcit.ca>
2022-03-18 13:46:58 -0700

    changed nodemailer tls

--- Util/mailer.js
+++ Util/mailer.js
@@ -1,33 +1,32 @@
-const nodemailer = require('nodemailer');
-const nodeEmail = process.env.nodeEmail
-const nodeEmailPassword = process.env.nodeEmailPassword
+const nodemailer = require("nodemailer");
+const nodeEmail = process.env.nodeEmail;
+const nodeEmailPassword = process.env.nodeEmailPassword;
 
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  tls: {
+    rejectUnauthorized: false,
+  },
+  auth: {
+    user: nodeEmail,
+    pass: nodeEmailPassword,
+  },
+});
 
-const transporter = nodemailer.createTransport(({
-    service: 'hotmail' ,
-    auth: {
-      user: nodeEmail,
-      pass: nodeEmailPassword
+const sendMail = (Sender, Receiver, subject, html) => {
+  const mailOptions = {
+    from: Sender,
+    to: Receiver,
+    subject: subject,
+    html: html,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    console.log(mailOptions);
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
     }
-  }));
-
-  const sendMail = (Sender, Receiver, subject, html)=>{
-    const mailOptions = {
-        from: Sender,
-        to: Receiver,
-        subject: subject,
-        html: html
-    }
-    transporter.sendMail(mailOptions, function (error, info) {
-      console.log(mailOptions)
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-
-  }
-  module.exports = sendMail;
-
-  
+  });
+};
+module.exports = sendMail;
