07bd4acc47e4829406d584301d16a2dca3c8b666
bkwaan <bkwan15@my.bcit.ca>
2022-03-17 18:27:04 -0700

    fixed for deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api", api);
 // Serve Staic Assets in production
 if (process.env.NODE_ENV === "production") {
   // Set Static folder
-  app.use(express.static("client/build"))
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
