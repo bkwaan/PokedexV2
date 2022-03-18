17e34d2ab13cf8fb3edbd24808323ad5cde6440c
bkwaan <bkwan15@my.bcit.ca>
2022-03-17 21:41:53 -0700

    initial commit

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ app.use(express.static(path.join(__dirname, "profilePics")));
 // Using api folder
 app.use("/api", api);
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("this server started on port " + PORT));
+
 // Serve Staic Assets in production
 if (process.env.NODE_ENV === "production") {
   // Set Static folder
-  app.use(express.static("client/build"))
+  app.use(express.static(path.join(__dirname,"client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-const PORT = process.env.PORT || 5000;
 
-console.log("Working")
-app.listen(PORT, () => console.log("this server started on port " + PORT));
