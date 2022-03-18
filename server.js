e1cdbf71ac6336636fb9d422e9110ead85881d95
bkwaan <bkwan15@my.bcit.ca>
2022-03-17 22:10:52 -0700

    fixed deployment variables

--- server.js
+++ server.js
@@ -21,13 +21,10 @@ app.listen(PORT, () => console.log("this server started on port " + PORT));
 // Serve Staic Assets in production
 if (process.env.NODE_ENV === "production") {
   // Set Static folder
-  app.use(express.static(path.join(__dirname,"client/build")));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve((__dirname = "client/build/index.html")));
+    res.sendFile(path.resolve((__dirname,"client/build/index.html")));
   });
 }
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/public/index.html"));
-  });
